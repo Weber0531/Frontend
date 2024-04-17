812960329e42ddf8243ad9e62694187a1dadb005
Weber0531 <ooooppppweber@gmail.com>
2024-04-18 02:09:07 +0800

    first commit

--- js/01_toggle.js
+++ js/01_toggle.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.button').click(function(event){
+    $('.button').click(function(){
         $('h1,p').toggle();// 自動偵測顯示或隱藏
     });
 })
---
+++ js/02_slide&fade.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $('.button1').click(function(event){
+        $('.message').slideDown(2000);// 數字為毫秒，1秒=1000毫秒
+    });
+
+    $('.button2').click(function(event){
+        $('.message').slideUp();// 數字為毫秒，1秒=1000毫秒
+    });
+    
+    $('.button3').click(function(event){
+        $('.message').slideToggle(1000);// 數字為毫秒，1秒=1000毫秒
+    });
+
+    $('.button4').click(function(event){
+        $('.message').fadeIn();// 數字為毫秒，1秒=1000毫秒
+    });
+
+    $('.button5').click(function(event){
+        $('.message').fadeOut(1000);// 數字為毫秒，1秒=1000毫秒
+    });
+
+    $('.button6').click(function(event){
+        $('.message').fadeToggle();// 數字為毫秒，1秒=1000毫秒
+    });
+
+    $('.start').click(function(event) {
+        $('.message').slideDown(3000).fadeOut().slideToggle()
+    })
+    // css寫法
+    $('.button7').click(function(event){
+        $('.text').addClass('active'); //add class="text active"
+    });
+
+    $('.button8').click(function(event){
+        $('.text').removeClass('active');//remove class="text active"
+    });
+
+    $('.button9').click(function(event){
+        $('.text').toggleClass('active');
+    });
+})
